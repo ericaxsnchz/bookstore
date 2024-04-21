3fcc8122ed0dcc8d62b2ecd1ef77c451d51da179
erica <erica.sanchez2835@yahoo.com>
2024-04-21 17:36:07 -0500

    add author and reviews models, link book to author, updated admin to match, create book detail page with url, view, and template

--- development/bookstore/store/models.py
+++ development/bookstore/store/models.py
@@ -1,10 +1,43 @@
 from django.db import models
-from django.utils import timezone
+import datetime 
+from django.contrib.auth.models import User
+
+class Author(models.Model):
+
+    first_name = models.CharField(max_length=100)
+
+    last_name = models.CharField(max_length=100) 
+
+ 
+
+    def __unicode__(self):
+
+        return "%s, %s" % (self.last_name, self.first_name)
+
+ 
 
 class Book(models.Model):
+
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    description = models.TextField()
-    publish_date = models.DateField(default=timezone.now)
+
+    author = models.ForeignKey('Author', on_delete=models.DO_NOTHING, blank=True, null=True)
+
+    description=models.TextField()
+
+    publish_date = models.DateField(default=datetime.date.today)
+
     price = models.DecimalField(decimal_places=2, max_digits=8)
+
     stock = models.IntegerField(default=0)
+
+ 
+
+class Review(models.Model):
+
+    book = models.ForeignKey('Book', on_delete=models.DO_NOTHING, blank=True, null=True)
+
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True)
+
+    publish_date = models.DateField(default=datetime.date.today)
+
+    text = models.TextField()
--- development/bookstore/store/urls.py
+++ development/bookstore/store/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from store import views
 
 urlpatterns = [
-    path('', views.store, name='index'),
+    path('', views.store, name='store'),
+    path('index', views.index, name='index'),
+    path('book/<int:book_id>', views.book_details, name='book_details')
 ]
